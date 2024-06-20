7bc4c424f72379a035045a7c0563027c9c46405d
Peter B. Golbus <pgolbus@gmail.com>
2024-06-20 08:35:12 -0400

    pick two hands before you pick two hands and then you pick two more

--- chopsticks/src/App.js
+++ chopsticks/src/App.js
@@ -5,30 +5,46 @@ import handStates from './handStates';
 function App() {
   const fingers = 5;
   const initialBoard = Array(9).fill('');
-  [0, 2, 6, 8].forEach(index => initialBoard[index] = 0);
+  [0, 2, 6, 8].forEach(index => initialBoard[index] = 1);
   const [board, setBoard] = useState(initialBoard);
-  // const [firstClick, setFirstClick] = useState(null);
+  const [firstClick, setFirstClick] = useState(null);
+  const [playerTurn, setPlayerTurn] = useState(0);
+
+  const flip = (turn) => {
+    setPlayerTurn(turn === 0 ? 1 : 0);
+  };
 
   const handleClick = (index) => {
+    console.log(board);
     const newBoard = [...board];
 
-    /* if (firstClick === null) {
+    if (firstClick === null) {
       // First click
+      if (newBoard[index] === 0) {
+        window.alert('Cannot start with a hand with no fingers');
+        return;
+      }
       setFirstClick(index);
     } else {
       // Second click
-      window.alert(`First Click: ${firstClick}, Second Click: ${index}`);
+      if (index === firstClick) {
+        window.alert('You must pick a different hand');
+        return;
+      }
+      // window.alert(`First Click: ${firstClick}, Second Click: ${index}`);
+
+      let current = newBoard[firstClick];
+      let newValue = (current + newBoard[index]) % fingers;
+      newBoard[index] = newValue;
+
       setFirstClick(null);
-      newBoard[firstClick] = '';
-      newBoard[index] = '';
-    }*/
 
-    let currentValueIndex = handStates.indexOf(newBoard[index]);
-    let newValueIndex = (currentValueIndex + 1) % fingers;
-    newBoard[index] = newValueIndex;
+      /* let current = newBoard[index];
+      let newValue = (current + 1) % fingers;
+      newBoard[index] = newValue; */
 
-    setBoard(newBoard);
-
+      setBoard(newBoard);
+    }
   };
 
   return (
