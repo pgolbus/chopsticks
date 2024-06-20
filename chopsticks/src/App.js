1bde8f8b1c6af8238fbfbe312e389585050b1bda
Peter B. Golbus <pgolbus@gmail.com>
2024-06-20 09:32:15 -0400

    takes turns

--- chopsticks/src/App.js
+++ chopsticks/src/App.js
@@ -5,13 +5,28 @@ import handStates from './handStates';
 function App() {
   const fingers = 5;
   const initialBoard = Array(9).fill('');
-  [0, 2, 6, 8].forEach(index => initialBoard[index] = 1);
+  const player1 = [0, 6];
+  const player2 = [2, 8];
+  const playerCells = [0, 2, 6, 8];
+  const player1TextCell = 3;
+  const player2TextCell = 5;
+  playerCells.forEach(index => initialBoard[index] = 1);
   const [board, setBoard] = useState(initialBoard);
   const [firstClick, setFirstClick] = useState(null);
   const [playerTurn, setPlayerTurn] = useState(0);
 
-  const flip = (turn) => {
-    setPlayerTurn(turn === 0 ? 1 : 0);
+  const flip = () => {
+    setPlayerTurn(playerTurn === 0 ? 1 : 0);
+  };
+
+  const renderCell = (cell, index) => {
+    if (playerCells.includes(index)) {
+      return handStates[cell];
+    } else if (index === player1TextCell) {
+      return 'Player 1';
+    } else if (index === player2TextCell) {
+      return 'Player 2';
+    }
   };
 
   const handleClick = (index) => {
@@ -23,6 +38,12 @@ function App() {
       if (newBoard[index] === 0) {
         window.alert('Cannot start with a hand with no fingers');
         return;
+      } else if ( 
+        (playerTurn === 0 && !player1.includes(index)) || 
+        (playerTurn === 1 && !player2.includes(index))
+      ) {
+        window.alert('You must start with one of your own hands');
+        return;
       }
       setFirstClick(index);
     } else {
@@ -30,19 +51,24 @@ function App() {
       if (index === firstClick) {
         window.alert('You must pick a different hand');
         return;
+      } else if (
+        (playerTurn === 0 && player1.includes(index)) || 
+        (playerTurn === 1 && player2.includes(index))
+      ) {
+        window.alert('I haven\'t built that yet');
+        return;
       }
       // window.alert(`First Click: ${firstClick}, Second Click: ${index}`);
+      /* let current = newBoard[index];
+      let newValue = (current + 1) % fingers;
+      newBoard[index] = newValue; */
 
       let current = newBoard[firstClick];
       let newValue = (current + newBoard[index]) % fingers;
       newBoard[index] = newValue;
 
       setFirstClick(null);
-
-      /* let current = newBoard[index];
-      let newValue = (current + 1) % fingers;
-      newBoard[index] = newValue; */
-
+      flip();
       setBoard(newBoard);
     }
   };
@@ -54,10 +80,10 @@ function App() {
         {board.map((cell, index) => (
           <div
             key={index}
-            className={`square ${[0, 2, 6, 8].includes(index) ? 'clickable' : ''}`}
-            onClick={() => [0, 2, 6, 8].includes(index) && handleClick(index)}
+            className={`square ${playerCells.includes(index) ? 'clickable' : ''}`}
+            onClick={() => playerCells.includes(index) && handleClick(index)}
           >
-            <pre>{[0, 2, 6, 8].includes(index) ? handStates[cell] : ''}</pre>
+            <pre>{renderCell(cell, index)}</pre>
           </div>
         ))}
       </div>
